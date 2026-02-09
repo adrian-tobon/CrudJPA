4f6a2596414830db051d0aa20cba26e4d4329549
adrian-tobon <catobonpe@gmail.com>
2026-02-09 05:07:35 -0500

    first commit

---
+++ src/test/java/com/curso/springboot/crud/jpa/CrudJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.crud.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
